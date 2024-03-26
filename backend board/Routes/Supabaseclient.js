a9ad1fa6231daeef1d0fbb13677ba3627ad9d40d
Sojal Mehra <sojalmehra0619@gmail.com>
2024-03-26 14:14:05 +0530

    gaurav

--- backend board/Routes/Supabaseclient.js
+++
@@ -1,9 +0,0 @@
-// supabaseClient.js
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.Supa_api_key 
-const supabaseKey = process.env.Supa_anon_key
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-module.exports = supabase;
