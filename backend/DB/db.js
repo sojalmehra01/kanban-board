2dd669114424042f8f79d51726f230ffef865c84
gaurav panwar <gp771234@gmail.com>
2024-04-15 16:05:20 +0530

    duplication fixed

--- backend/DB/db.js
+++ backend/DB/db.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const mongoUrl = process.env.MONGO_URL;
 
 const connectDB = async () => {
-    await mongoose.connect(mongoUrl)
+    await mongoose.connect(mongoUrl, {
+        autoIndex: true, //make this also true
+    })
     .then(() => console.log('Connected'))
     .catch(err => console.error('Could not connect to MongoDB', err));
 };
--- backend/Models/Board.js
+++ backend/Models/Board.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const boardSchema = new mongoose.Schema({
  board_title: {
@@ -52,6 +53,6 @@ const boardSchema = new mongoose.Schema({
 //  next();
 // });
 
-
+boardSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Board', boardSchema);
