44f5a2b8d253cfcbd4832ef63853443ac9b59afc
gaurav panwar <gp771234@gmail.com>
2024-05-25 21:17:43 +0530

    als;kdfj

--- frontend/src/chat-window/Chatmodal.jsx
+++ frontend/src/chat-window/Chatmodal.jsx
@@ -8,12 +8,14 @@ const Chatmodal = ({ isOpen, onClose, children }) => {
         <div className="overlay">
           <div className="overlay__background" onClick={onClose} />
           <div className="overlay__container">
+            {/* //is div mai likhna hai code aappko sojal bhaiya (frontend ka) */}
             <div className="overlay__controls">
               <button
                 className="overlay__close"
                 type="button"
                 onClick={onClose}
               />
+
             </div>
             {children}
           </div>
