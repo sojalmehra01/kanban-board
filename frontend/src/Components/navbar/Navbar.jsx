327118943507828d156ad24314bdd328427334b1
gaurav panwar <gp771234@gmail.com>
2024-06-27 11:01:29 +0530

    a;ldskjf

--- frontend/src/Components/navbar/Navbar.jsx
+++ frontend/src/Components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./navbar.css";
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-
+//navbar
 
   return (
     <div className='navbar'>
@@ -15,7 +15,7 @@ const Navbar = () => {
         <Link className='link_btn' to={"/login"}>Login</Link>
         <Link className='link_btn' to={"/signup"}>signup</Link>
         <Link onClick={()=>{localStorage.removeItem('sb-tkxtjervogiccudypfwz-auth-token')}} className='link_btn' to={"/login"}>logout</Link>
-        <Link className='link_btn' to={"/dashboard"}>home</Link>
+        {/* <Link className='link_btn' to={"/dashboard"}>home</Link> */}
         <div className="user"></div>
     </div>
       </div>
